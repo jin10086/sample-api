be306b6ac9cd05fae7edf8d6405da16ba32f6ecb
jin10086 <1019855680@qq.com>
2019-01-05 22:58:36 +0800

    init

---
+++ app.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import, unicode_literals
+
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+from flask_caching import Cache
+from flask_pymongo import PyMongo
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app, origins=["http://localhost:8080"])
+app.config.from_pyfile("config.py")
+api = Api(app)
+mongo = PyMongo(app)
+cache = Cache(app, config={"CACHE_TYPE": "redis"})
+
+
+class Api(Resource):
+
+    def get(self):
+        return {"hello": "world"}
+
+
+api.add_resource(Api, "/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000, threaded=True)
---
+++ readme.md
@@ -0,0 +1,24 @@
+## 一个简单的使用flask 搭建Api的项目
+
+主要目的是快速开发...
+
+后续项目都可以基于此开发.
+
+
+## 使用了以下依赖
+
+- [flask](http://flask.pocoo.org/)
+- [flask-caching](https://flask-caching.readthedocs.io/en/latest/)
+- [flask-pymongo](https://flask-pymongo.readthedocs.io/)
+- [flask-cors](https://flask-cors.readthedocs.io/en/latest/)
+- [flask-restful](https://flask-restful.readthedocs.io/)
+
+## 如何使用
+
+`virtualenv --no-site-packages venv`
+
+`source venv/bin/activate`
+
+`pip install -r requirements.txt`
+
+`python app.py`
