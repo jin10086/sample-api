92b0f871cdfeac5d09368301b95a5361958ea8f2
jin10086 <igaojin@qq.com>
2019-05-29 14:12:27 +0800

    Revert "go"
    This reverts commit f2c064b6ee8dd741ffa92664639f8b77190d8dde.

--- app.py
+++ app.py
@@ -2,30 +2,26 @@ from __future__ import absolute_import, unicode_literals
 
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
+from flask_caching import Cache
 from flask_pymongo import PyMongo
 from flask_cors import CORS
-from flask_redis import FlaskRedis
 
-from ut import check
 
 app = Flask(__name__)
 CORS(app, origins=["http://localhost:8080"])
 app.config.from_pyfile("config.py")
 api = Api(app)
 mongo = PyMongo(app)
-redis_store = FlaskRedis(app)
+cache = Cache(app, config={"CACHE_TYPE": "redis"})
 
 
 class Api(Resource):
 
-    def get(self, address):
-        if redis_store.get(address):
-            return {"status": 0, "msg": "请不要点击太快."}
-        redis_store.set(address, 1, 10)
-        return check(address)
+    def get(self):
+        return {"hello": "world"}
 
 
-api.add_resource(Api, "/<string:address>")
+api.add_resource(Api, "/")
 
 
 if __name__ == "__main__":
